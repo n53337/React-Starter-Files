5113a1e43ef43107874cd315d3b69761492d6c8c
u53f <scalewithusef@gmail.com>
2022-11-14 11:57:37 +0000

    remove useless files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <h1>
+      React Starting Files by
+      <a href="https://github.com/n53337/" target={_blank}>
+        @n5333f
+      </a>
+    </h1>
+  );
+}
+
+export default App;
